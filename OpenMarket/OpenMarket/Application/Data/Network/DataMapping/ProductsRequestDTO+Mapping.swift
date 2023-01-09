5cb5f9557ef946cab35b3ec97ffc257431f5d79c
derrickkim0109 <derrickkim0109@naver.com>
2023-01-09 23:11:30 +0900

    refactor: Product Mapping DTO 이름 변경

---
+++ OpenMarket/OpenMarket/Application/Data/Network/DataMapping/ProductsRequestDTO+Mapping.swift
@@ -0,0 +1,45 @@
+//
+//  ProductsRequestDTO.swift
+//  OpenMarket
+//
+//  Created by 데릭, 수꿍.
+//
+
+import Foundation
+
+struct ProductsRequestDTO: Codable {
+    let pages: [Product]
+}
+
+enum Currency: String, Codable {
+    case krw = "KRW"
+    case usd = "USD"
+}
+
+struct Product: Codable {
+    let id: Int
+    let vendorId: Int
+    let name: String
+    let thumbnail: String
+    let currency: Currency
+    let price: Double
+    let bargainPrice: Double
+    let discountedPrice: Double
+    let stock: Int
+    let createdAt: String
+    let issuedAt: String
+}
+
+extension Product {
+    func toDomain() -> ProductEntity {
+        return ProductEntity(
+            id: id,
+            vendorID: vendorId,
+            thumbnail: thumbnail,
+            name: name,
+            currency: currency.rawValue,
+            originalPrice: price,
+            discountedPrice: bargainPrice,
+            stock: stock)
+    }
+}
