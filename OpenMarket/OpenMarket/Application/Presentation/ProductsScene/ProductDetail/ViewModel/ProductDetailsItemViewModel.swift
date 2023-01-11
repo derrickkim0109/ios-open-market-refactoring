e82a0b320bae57cede72e2cd393544f64ea2cabd
derrickkim0109 <derrickkim0109@naver.com>
2023-01-11 22:53:10 +0900

    feat: ProductDetailsItemViewModel 생성

---
+++ OpenMarket/OpenMarket/Application/Presentation/ProductsScene/ProductDetail/ViewModel/ProductDetailsItemViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ProductDetailsItemViewModel.swift
+//  OpenMarket
+//
+//  Created by Derrick kim on 2023/01/11.
+//
+
+import Foundation
+
+final class ProductDetailsItemViewModel {
+    private let model: ProductDetailsEntity
+    
+    init(model: ProductDetailsEntity) {
+        self.model = model
+    }
+
+    func returnTotalPage(_ index: Int) -> String {
+        return "\(index)/\(model.images.count)"
+    }
+}
