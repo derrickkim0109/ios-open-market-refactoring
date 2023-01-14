7092609a0e06fdb692595483e627e14929bb4c71
derrickkim0109 <derrickkim0109@naver.com>
2023-01-14 23:18:44 +0900

    feat: TypedProductDetailsRequestDTO 프로토콜 채택 타입 변경

--- OpenMarket/OpenMarket/Application/Data/Network/DataMapping/TypedProductDetailsRequestDTO.swift
+++ OpenMarket/OpenMarket/Application/Data/Network/DataMapping/TypedProductDetailsRequestDTO.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct TypedProductDetailsRequestDTO: Codable {
+struct TypedProductDetailsRequestDTO: Encodable {
     let name: String?
     let descriptions: String?
     let price: Double?
