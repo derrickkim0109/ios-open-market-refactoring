60088585db5da1d02161511b3915ac3ff2ef2344
derrickkim0109 <derrickkim0109@naver.com>
2023-01-15 18:35:24 +0900

    feat: ProductEnrollmentViewModelActions 생성

---
+++ OpenMarket/OpenMarket/Application/Presentation/ProductsScene/ProductEnrollment/ViewModel/Actions/ProductEnrollmentViewModelActions.swift
@@ -0,0 +1,10 @@
+//
+//  ProductEnrollmentViewModelActions.swift
+//  OpenMarket
+//
+//  Created by Derrick kim on 2023/01/14.
+//
+
+struct ProductEnrollmentViewModelActions {
+    let dismissViewController: () -> Void
+}
