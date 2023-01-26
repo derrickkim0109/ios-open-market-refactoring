0e4d9304dd69cc419361b6ba5686181b4f0f749d
derrickkim0109 <derrickkim0109@naver.com>
2023-01-26 19:46:07 +0900

    feat: NetworkServiceMock 생성
    - NetworkSessionManagerMock 생성

---
+++ OpenMarket/OpenMarketTests/Infrastructure/Network/Mocks/NetworkServiceMocks.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkServiceMocks.swift
+//  OpenMarketTests
+//
+//  Created by Derrick kim on 2023/01/24.
+//
+
+import Foundation
+
+class NetworkConfigurableMock: NetworkConfigurable {
+    var baseURL: URL = URL(string: "https://openmarket.yagom-academy.kr")!
+    var headers: [String: String] = [:]
+    var queryParameters: [String: String] = [:]
+}
---
+++ OpenMarket/OpenMarketTests/Infrastructure/Network/Mocks/NetworkSessionManagerMock.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkSessionManagerMock.swift
+//  OpenMarketTests
+//
+//  Created by Derrick kim on 2023/01/24.
+//
+
+import Foundation
+
+struct NetworkSessionManagerMock: NetworkSessionManager {
+    let response: HTTPURLResponse?
+    let data: Data?
+    let error: Error?
+
+    func request(_ request: URLRequest) async throws -> Data {
+        if let statusCode = response?.statusCode,
+           statusCode != 200 {
+            guard statusCode != 202 else {
+                return data!
+            }
+            throw NetworkError.error(statusCode: statusCode, data: data)
+        }
+
+        if let error = error {
+            throw resolve(error: error)
+        }
+        return data!
+    }
+
+    private func resolve(error: Error) -> NetworkError {
+        let code = URLError.Code(rawValue: (error as NSError).code)
+        switch code {
+        case .notConnectedToInternet:
+            return .notConnected
+        case .cancelled:
+            return .cancelled
+        default:
+            return .generic(error)
+        }
+    }
+}
