792b2a3dc2a5bbc112aa5f6ae231853fd06b7610
derrickkim0109 <derrickkim0109@naver.com>
2023-02-01 22:39:55 +0900

    feat: ProductDetailsEntity stub 생성 메서드 추가
    - ProductEntity stub 생성 메서드의 인수 default값 추가

---
+++ OpenMarket/OpenMarketTests/Presentation/ProductsScene/Mocks/ProductDetailsEntity+Stub.swift
@@ -0,0 +1,37 @@
+//
+//  ProductDetailsEntity+Stub.swift
+//  OpenMarketTests
+//
+//  Created by Derrick kim on 2023/02/01.
+//
+
+import Foundation
+
+extension ProductDetailsEntity {
+    static func stub(
+        id: Int = 1,
+        vendorID: Int = 12,
+        name: String = "애플 맥북",
+        description: String = "미국산",
+        currency: Currency = .krw,
+        price: Double = 1000000.0,
+        bargainPrice: Double = 1000.0,
+        stock: Int = 30,
+        images: [String]) -> Self {
+            return ProductDetailsEntity(
+                id: id,
+                vendorID: vendorID,
+                name: name,
+                description: description,
+                currency: currency,
+                price: price,
+                bargainPrice: bargainPrice,
+                stock: stock,
+                images: images)
+        }
+
+    static let sample = ProductDetailsEntity.stub(
+        images: ["apple.png", "iphone.png"])
+}
+
+
---
+++ OpenMarket/OpenMarketTests/Presentation/ProductsScene/Mocks/ProductEntity+Stub.swift
@@ -0,0 +1,32 @@
+//
+//  ProductEntity+Stub.swift
+//  OpenMarketTests
+//
+//  Created by Derrick kim on 2023/01/31.
+//
+
+import Foundation
+
+extension ProductEntity {
+    static func stub(
+        id: Int = 1,
+        vendorID: Int = 12,
+        thumbnail: String = "image.png",
+        name: String = "맥북",
+        currency: String = "krw",
+        originalPrice: Double = 1000000.0,
+        discountedPrice: Double = 1000.0,
+        stock: Int = 120) -> Self {
+            return ProductEntity(
+                id: id,
+                vendorID: vendorID,
+                thumbnail: thumbnail,
+                name: name,
+                currency: currency,
+                originalPrice: originalPrice,
+                discountedPrice: discountedPrice,
+                stock: stock)
+        }
+
+    static let sample = ProductEntity.stub()
+}
